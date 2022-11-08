904c3b371d653c362451c389dc5f84f69b243861
speedjin1995 <speedjin1995@gmail.com>
2022-11-08 12:14:14 +0800

    changed dataset limits

--- php/datasets.php
+++ php/datasets.php
@@ -4,13 +4,31 @@ require_once 'db_connect.php';
 if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
     $startDate = filter_input(INPUT_POST, 'startDate', FILTER_SANITIZE_STRING);
     $fromDate = new DateTime($startDate);
-    $start = date_format($fromDate, "Y-m-d H:i:s");
+    $start = date_format($fromDate, "Y-m-d 00:00:00");
 	$endDate = filter_input(INPUT_POST, 'endDate', FILTER_SANITIZE_STRING);
     $toDate = new DateTime($endDate);
     $end= date_format($toDate,"Y-m-d 23:59:59");
     $duration = filter_input(INPUT_POST, 'duration', FILTER_SANITIZE_STRING);
+    $limit = 30;
 
-    $empQuery = "SELECT * from reading WHERE timestamp >= '".$start."' AND timestamp <= '".$end."' ORDER BY timestamp";
+    if($duration == 1){
+        $limit = 60;
+    }
+    else if($duration == 5){
+        $limit = 150;
+    }
+    else if($duration == 10){
+        $limit = 300;
+    }
+    else if($duration == 30){
+        $limit = 900;
+    }
+    else if($duration == 60){
+        $limit = 1800;
+    }
+
+
+    $empQuery = "SELECT * from reading WHERE timestamp >= '".$start."' AND timestamp <= '".$end."' ORDER BY timestamp DESC LIMIT ".$limit;
     $empRecords = mysqli_query($db, $empQuery);
     $data = array();
     $count = 0;
@@ -18,8 +36,11 @@ if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
     if($duration == 1){
         while($row = mysqli_fetch_assoc($empRecords)) {
             if($count % 2 == 0){
+                $fromDate = new DateTime($row['timestamp']);
+                $start = date_format($fromDate, "Y-m-d H:i");
+
                 $data[] = array( 
-                    "timestamp"=>$row['timestamp'],
+                    "timestamp"=>$start,
                     "aircond_1"=>$row['aircond_1'],
                     "aircond_2"=>$row['aircond_2'],
                     "chamber"=>$row['chamber'],
@@ -43,8 +64,11 @@ if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
     else if($duration == 5){
         while($row = mysqli_fetch_assoc($empRecords)) {
             if($count % 10 == 0){
+                $fromDate = new DateTime($row['timestamp']);
+                $start = date_format($fromDate, "Y-m-d H:i");
+
                 $data[] = array( 
-                    "timestamp"=>$row['timestamp'],
+                    "timestamp"=>$start,
                     "aircond_1"=>$row['aircond_1'],
                     "aircond_2"=>$row['aircond_2'],
                     "chamber"=>$row['chamber'],
@@ -68,8 +92,11 @@ if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
     else if($duration == 10){
         while($row = mysqli_fetch_assoc($empRecords)) {
             if($count % 20 == 0){
+                $fromDate = new DateTime($row['timestamp']);
+                $start = date_format($fromDate, "Y-m-d H:i");
+
                 $data[] = array( 
-                    "timestamp"=>$row['timestamp'],
+                    "timestamp"=>$start,
                     "aircond_1"=>$row['aircond_1'],
                     "aircond_2"=>$row['aircond_2'],
                     "chamber"=>$row['chamber'],
@@ -93,8 +120,11 @@ if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
     else if($duration == 30){
         while($row = mysqli_fetch_assoc($empRecords)) {
             if($count % 60 == 0){
+                $fromDate = new DateTime($row['timestamp']);
+                $start = date_format($fromDate, "Y-m-d H:i");
+
                 $data[] = array( 
-                    "timestamp"=>$row['timestamp'],
+                    "timestamp"=>$start,
                     "aircond_1"=>$row['aircond_1'],
                     "aircond_2"=>$row['aircond_2'],
                     "chamber"=>$row['chamber'],
@@ -118,8 +148,11 @@ if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
     else if($duration == 60){
         while($row = mysqli_fetch_assoc($empRecords)) {
             if($count % 120 == 0){
+                $fromDate = new DateTime($row['timestamp']);
+                $start = date_format($fromDate, "Y-m-d H:i");
+
                 $data[] = array( 
-                    "timestamp"=>$row['timestamp'],
+                    "timestamp"=>$start,
                     "aircond_1"=>$row['aircond_1'],
                     "aircond_2"=>$row['aircond_2'],
                     "chamber"=>$row['chamber'],
@@ -141,10 +174,12 @@ if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
         }
     }
 
+    $data2 = array_reverse($data,true);
+
     echo json_encode(
         array(
             "status"=> "success", 
-            "message"=> $data
+            "message"=> $data2
         )
     );
 }
