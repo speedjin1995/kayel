47869e922e91c8903d0ea9f834b54220f23bd5e9
speedjin1995 <speedjin1995@gmail.com>
2022-11-12 11:25:33 +0800

    Changed duration

--- php/datasets.php
+++ php/datasets.php
@@ -11,19 +11,19 @@ if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
     $duration = filter_input(INPUT_POST, 'duration', FILTER_SANITIZE_STRING);
     $limit = 30;
 
-    if($duration == 1){
-        $limit = 60;
-    }
-    else if($duration == 5){
+    if($duration == 5){
         $limit = 300;
-    }
-    else if($duration == 10){
-        $limit = 600;
     }
     else if($duration == 30){
         $limit = 1800;
     }
     else if($duration == 60){
+        $limit = 7200;
+    }
+    else if($duration == 240){
+        $limit = 1800;
+    }
+    else if($duration == 1440){
         $limit = 7200;
     }
 
@@ -33,69 +33,9 @@ if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
     $data = array();
     $count = 0;
 
-    if($duration == 1){
-        while($row = mysqli_fetch_assoc($empRecords)) {
-            if($count % 2 == 0){
-                $start = date("Y-m-d H:i", strtotime('+8 hours',strtotime($row['timestamp'])));
-                //$start = date_format($fromDate, "Y-m-d H:i");
-
-                $data[] = array( 
-                    "timestamp"=>$start,
-                    "aircond_1"=>$row['aircond_1'],
-                    "aircond_2"=>$row['aircond_2'],
-                    "chamber"=>$row['chamber'],
-                    "hot_water_1"=>$row['hot_water_1'],
-                    "hot_water_2"=>$row['hot_water_2'],
-                    "hot_water_3"=>$row['hot_water_3'],
-                    "length"=>$row['length'],
-                    "rpm"=>$row['rpm'],
-                    "batch"=>$row['batch'],
-                    "length_saved"=>$row['length_saved'],
-                    "sensor_1"=>$row['sensor_1'],
-                    "sensor_2"=>$row['sensor_2'],
-                    "sensor_3"=>$row['sensor_3'],
-                    "layer"=>$row['layer'],
-                    "width_1"=>$row['width_1'],
-                    "width_2"=>$row['width_2']
-                );
-            }
-
-            $count = $count + 1;
-        }
-    }
-    else if($duration == 5){
+    if($duration == 5){
         while($row = mysqli_fetch_assoc($empRecords)) {
             if($count % 10 == 0){
-                $start = date("Y-m-d H:i", strtotime('+8 hours',strtotime($row['timestamp'])));
-                //$start = date_format($fromDate, "Y-m-d H:i");
-
-                $data[] = array( 
-                    "timestamp"=>$start,
-                    "aircond_1"=>$row['aircond_1'],
-                    "aircond_2"=>$row['aircond_2'],
-                    "chamber"=>$row['chamber'],
-                    "hot_water_1"=>$row['hot_water_1'],
-                    "hot_water_2"=>$row['hot_water_2'],
-                    "hot_water_3"=>$row['hot_water_3'],
-                    "length"=>$row['length'],
-                    "rpm"=>$row['rpm'],
-                    "batch"=>$row['batch'],
-                    "length_saved"=>$row['length_saved'],
-                    "sensor_1"=>$row['sensor_1'],
-                    "sensor_2"=>$row['sensor_2'],
-                    "sensor_3"=>$row['sensor_3'],
-                    "layer"=>$row['layer'],
-                    "width_1"=>$row['width_1'],
-                    "width_2"=>$row['width_2']
-                );
-            }
-
-            $count = $count + 1;
-        }
-    }
-    else if($duration == 10){
-        while($row = mysqli_fetch_assoc($empRecords)) {
-            if($count % 20 == 0){
                 $start = date("Y-m-d H:i", strtotime('+8 hours',strtotime($row['timestamp'])));
                 //$start = date_format($fromDate, "Y-m-d H:i");
 
@@ -156,6 +96,66 @@ if(isset($_POST['startDate'], $_POST['endDate'], $_POST['duration'])){
     else if($duration == 60){
         while($row = mysqli_fetch_assoc($empRecords)) {
             if($count % 120 == 0){
+                $start = date("Y-m-d H:i", strtotime('+8 hours',strtotime($row['timestamp'])));
+                //$start = date_format($fromDate, "Y-m-d H:i");
+
+                $data[] = array( 
+                    "timestamp"=>$start,
+                    "aircond_1"=>$row['aircond_1'],
+                    "aircond_2"=>$row['aircond_2'],
+                    "chamber"=>$row['chamber'],
+                    "hot_water_1"=>$row['hot_water_1'],
+                    "hot_water_2"=>$row['hot_water_2'],
+                    "hot_water_3"=>$row['hot_water_3'],
+                    "length"=>$row['length'],
+                    "rpm"=>$row['rpm'],
+                    "batch"=>$row['batch'],
+                    "length_saved"=>$row['length_saved'],
+                    "sensor_1"=>$row['sensor_1'],
+                    "sensor_2"=>$row['sensor_2'],
+                    "sensor_3"=>$row['sensor_3'],
+                    "layer"=>$row['layer'],
+                    "width_1"=>$row['width_1'],
+                    "width_2"=>$row['width_2']
+                );
+            }
+
+            $count = $count + 1;
+        }
+    }
+    else if($duration == 240){
+        while($row = mysqli_fetch_assoc($empRecords)) {
+            if($count % 480 == 0){
+                $start = date("Y-m-d H:i", strtotime('+8 hours',strtotime($row['timestamp'])));
+                //$start = date_format($fromDate, "Y-m-d H:i");
+
+                $data[] = array( 
+                    "timestamp"=>$start,
+                    "aircond_1"=>$row['aircond_1'],
+                    "aircond_2"=>$row['aircond_2'],
+                    "chamber"=>$row['chamber'],
+                    "hot_water_1"=>$row['hot_water_1'],
+                    "hot_water_2"=>$row['hot_water_2'],
+                    "hot_water_3"=>$row['hot_water_3'],
+                    "length"=>$row['length'],
+                    "rpm"=>$row['rpm'],
+                    "batch"=>$row['batch'],
+                    "length_saved"=>$row['length_saved'],
+                    "sensor_1"=>$row['sensor_1'],
+                    "sensor_2"=>$row['sensor_2'],
+                    "sensor_3"=>$row['sensor_3'],
+                    "layer"=>$row['layer'],
+                    "width_1"=>$row['width_1'],
+                    "width_2"=>$row['width_2']
+                );
+            }
+
+            $count = $count + 1;
+        }
+    }
+    else if($duration == 1440){
+        while($row = mysqli_fetch_assoc($empRecords)) {
+            if($count % 2880 == 0){
                 $start = date("Y-m-d H:i", strtotime('+8 hours',strtotime($row['timestamp'])));
                 //$start = date_format($fromDate, "Y-m-d H:i");
 
