6ef69d1aac840206872957d1193435c181ae74de
speedjin1995 <speedjin1995@gmail.com>
2022-11-28 19:34:09 +0800

    added connect straight to server

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,6 +1,7 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
 $db = mysqli_connect("localhost", "root", "Aaa111222333", "kayel");
+//$db = mysqli_connect("sql364.main-hosting.eu", "u782565293_kayel", "Aaa111222333", "u782565293_kayel");
 
 if(mysqli_connect_errno()){
     echo 'Database connection failed with following errors: ' . mysqli_connect_error();
